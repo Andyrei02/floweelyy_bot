1ec5ac58adea29c7b7ad2e51f63aa68b953ae3d3
Andyrei02 <acenusa58@gmail.com>
2025-08-30 16:46:40 +0300

    update var name for database

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DB_URL = os.getenv("DB_URL")  # postgresql://user:pass@host:port/dbname
+DB_URL = os.getenv("DATABASE_URL")  # postgresql://user:pass@host:port/dbname
 ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",") if x]
